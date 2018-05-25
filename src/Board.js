e8b7533597264f0dbcdc0de9c0d742a4a1c9f5df
Tetevemasque <tetevemasque@gmail.com>
2018-05-25 03:26:03 +0200

    Jeu fonctionnel au clavier.

--- src/Board.js
+++ src/Board.js
@@ -19,7 +19,7 @@ class Board
         {
             u = 1
             while (u <= this.size) {
-                this.pions.push({'x' : i, 'y' : u, 'nb' : 0})
+                this.pions.push({'index' : (i - 1), 'x' : i, 'y' : u, 'nb' : 0, 'fusionnable' : true})
                 u++
 
             }
@@ -27,8 +27,9 @@ class Board
             i++
         }
 
-        console.log(this.pions)
+        // console.log(this.pions)
 
+        this.ajoutPion()
         this.ajoutPion()
 
     }
@@ -41,29 +42,461 @@ class Board
         let placesLibres = this.getPlacesLibres()
 
         if (placesLibres.length > 0) {
-            let x = this.getChiffreRandom()
-            let y = this.getChiffreRandom()
 
-            let placeIndex = this.getPlaceIndex(x, y, placesLibres)
-            this.pions[placeIndex].nb = 2
-            console.log(this.pions[placeIndex])
+            // choper l'index de la position correspondant à une choisie aléatoirement parmi celles qui sont libres
+   
+            // console.log('intervale : 0, ' + (placesLibres.length - 1))
+            let randomIndex = this.getChiffreRandom(0, (placesLibres.length - 1))
 
-            
+            let x = placesLibres[randomIndex].x
+            let y = placesLibres[randomIndex].y
+
+            this.setPlaceNb(x, y, 2)
+            // console.log('nouveau pion : ' + x + ':' + y)        
             
         } else {
-            console.log('FIN')
+            // console.log('FIN')
 
         }
     }
 
     getPlacesLibres()
     {
-        return this.pions.filter(pion => pion.nb == 0)
+        let libres = this.pions.filter(pion => pion.nb == 0)
+        let strlibres = 'Libres :'
+        libres.forEach(element => {
+            strlibres = strlibres + element.x + ':' + element.y + ', '
+        });
+        // console.log(strlibres)
+
+        return libres
+    }
+
+    getPlacesPrises()
+    {
+        let prises = this.pions.filter(pion => pion.nb != 0)
+        let strlibres = 'Prises :'
+        prises.forEach(element => {
+            strlibres = strlibres + element.x + ':' + element.y + ', '
+        });
+        // console.log(strlibres)
+
+        return prises
+    }
+
+    getPlaceStatut(x, y)
+    {
+        // console.log('TEST ' + x + ':' + y)
+        let index = this.getPlaceIndex(x, y)
+        // console.log("type")
+        // console.log(typeof(index))
+        if (typeof(index) == 'number') { // la place existe, on a son index
+            // console.log("index n'est pas false mais il est " + typeof(index) + " et sa veleur est : " + index)
+            if (this.pions[index].nb == 0) { // l'attribut nb est à 0 = il est libre
+                return true;
+
+            } else { // l'attribut nb n'est pas à 0
+                // console.log("pas libre car nb = " + this.pions[index].nb)
+                return false;
+
+            }
+
+        } else { // pas de place, pas d'index, pas de chocolats
+            // console.log('pas libre car index indisponible')
+            return false;
+
+        }
     }
 
     getPlaceIndex(x, y, places = this.pions)
     {
-        return places.findIndex(place => place.x == x && place.y == y)
+        let index = this.pions.findIndex(place => place.x == x && place.y == y)
+        // console.log("GET INDEX POUR " + x + ":" + y)
+        // console.log(index)
+        if (typeof(index) == 'number' && index >= 0 && index <= (this.pions.length - 1)) {
+            //index = this.pions.filter(place => place.x == x && place.y == y)[0].index
+
+            // console.log("index : " + index)
+            return index;
+
+        } else {
+            // console.log('ah ! ' + index)
+            return false
+
+        }
+    }
+
+    getPlaceNb(x, y) {
+        let index = this.getPlaceIndex(x, y)
+        if (typeof(index) == 'number') {
+            return this.pions[index].nb
+
+        } else {
+            return false;
+
+        }
+    }
+
+    setPlaceNb(x, y, newNb) {
+        let index = this.getPlaceIndex(x, y)
+        if (typeof(index) == 'number') {
+            this.pions[index].nb = newNb
+
+        }
+    }
+
+    getFusionnable(x, y) {
+        let index = this.getPlaceIndex(x, y)
+        if (typeof(index) == 'number') {
+            return this.pions[index].fusionnable
+
+        } else {
+            return false;
+            
+        }
+    }
+
+    setFusionnable(x, y, newFusionnable = true) {
+        let index = this.getPlaceIndex(x, y)
+        if (typeof(index) == 'number') {
+            this.pions[index].fusionnable = newFusionnable
+
+        }
+
+    }
+
+    fusionnerPlaces(x1, y1, x2, y2) {
+        this.setPlaceNb(x1, y1, 0)
+        this.setPlaceNb(x2, y2, (this.getPlaceNb(x2, y2) * 2))
+
+    }
+
+    sortByYDesc(a,b) {
+        if (a.y < b.y) {
+            return -1;
+
+        }
+
+        if (a.y > b.y) {
+            return 1;
+
+        }
+        this.ajoutPion()
+        return 0;
+
+    }
+
+    changerPosition(oldX, oldY, newX, newY) {
+        let nb = this.getPlaceNb(oldX, oldY)
+        this.setPlaceNb(oldX, oldY, 0)
+        this.setPlaceNb(newX, newY, nb)
+        // console.log("changement de valeur pour " + newX + ":" + newY + " - nb = " + nb)
+
+    }
+
+    deplacer(sens) {
+
+        if (sens == 'h')
+        {
+
+            let liste = this.getPlacesPrises().sort(function(a,b){
+                if (a.x > b.x) {
+                    return 1
+                } else if (a.x < b.x) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            let deplacement = false
+            // console.log(liste[0].nb);
+
+            liste.forEach(element => {
+                let libre = true
+                let oldPosX = element.x
+                let oldPosY = element.y
+                let currentPosX = element.x
+                let currentPosY = element.y
+                let newPosX = element.x
+                let newPosY = element.y
+                let currentNb = this.getPlaceNb(element.x, element.y)
+
+                this.setFusionnable(element.x, element.y, true)
+                // Bon alors là ce qu'on va faire c'est qu'on va placer le "pion" le plus à droite possible
+
+                while (libre == true) {
+                    libre = this.getPlaceStatut((currentPosX - 1), currentPosY)
+                    // console.log(currentPosX + ":" + (currentPosY - 1) + " libre ? " + libre)
+                    if (libre == true) {
+                        // console.log('oui')
+
+                        newPosX = newPosX - 1
+                        currentPosX = currentPosX - 1
+
+                    } // console.log(libre)
+
+                }
+                // console.log("sortie du while. position retenue " + newPosX + ":" + newPosY)
+                // et la on change la position
+                this.changerPosition(oldPosX, oldPosY, newPosX, newPosY)
+                // console.log(oldPosX + ":" + oldPosY + " > " + newPosX + ":" + newPosY + " " + this.getPlaceStatut(1, 1) + " " + this.pions[0].nb)
+
+                if (oldPosX != newPosX || oldPosY != newPosY) {
+                    deplacement = true
+                }
+
+                // on vérifie si la case d'après n'aurait pas le même chiffre
+                // console.log(newPosX + ":" + (newPosY + 1) + " est-il fusionnable ?")
+                // console.log(this.getFusionnable(newPosX, (newPosY + 1)))
+                if (
+                    this.getPlaceNb((newPosX - 1), newPosY ) == currentNb && this.getFusionnable((newPosX - 1), newPosY) == true
+                )
+                {
+                    this.fusionnerPlaces(newPosX, newPosY, (newPosX - 1), newPosY )
+                    // console.log("fuuuuuuusion")
+                    this.setFusionnable((newPosX - 1), newPosY, false)
+
+                }
+
+            });
+
+            if (deplacement == true) {
+                this.ajoutPion()
+
+            }
+        }
+
+        if (sens == 'b')
+        {
+
+            let liste = this.getPlacesPrises().sort(function(a,b){
+                if (a.x < b.x) {
+                    return 1
+                } else if (a.x > b.x) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            let deplacement = false
+            // console.log(liste[0].nb);
+
+            liste.forEach(element => {
+                let libre = true
+                let oldPosX = element.x
+                let oldPosY = element.y
+                let currentPosX = element.x
+                let currentPosY = element.y
+                let newPosX = element.x
+                let newPosY = element.y
+                let currentNb = this.getPlaceNb(element.x, element.y)
+
+                this.setFusionnable(element.x, element.y, true)
+                // Bon alors là ce qu'on va faire c'est qu'on va placer le "pion" le plus à droite possible
+
+                while (libre == true) {
+                    libre = this.getPlaceStatut((currentPosX + 1), currentPosY)
+                    // console.log(currentPosX + ":" + (currentPosY - 1) + " libre ? " + libre)
+                    if (libre == true) {
+                        // console.log('oui')
+
+                        newPosX = newPosX + 1
+                        currentPosX = currentPosX + 1
+
+                    } // console.log(libre)
+
+                }
+                // console.log("sortie du while. position retenue " + newPosX + ":" + newPosY)
+                // et la on change la position
+                this.changerPosition(oldPosX, oldPosY, newPosX, newPosY)
+                // console.log(oldPosX + ":" + oldPosY + " > " + newPosX + ":" + newPosY + " " + this.getPlaceStatut(1, 1) + " " + this.pions[0].nb)
+
+                if (oldPosX != newPosX || oldPosY != newPosY) {
+                    deplacement = true
+                }
+
+                // on vérifie si la case d'après n'aurait pas le même chiffre
+                // console.log(newPosX + ":" + (newPosY + 1) + " est-il fusionnable ?")
+                // console.log(this.getFusionnable(newPosX, (newPosY + 1)))
+                if (
+                    this.getPlaceNb((newPosX + 1), newPosY ) == currentNb && this.getFusionnable((newPosX - 1), newPosY) == true
+                )
+                {
+                    this.fusionnerPlaces(newPosX, newPosY, (newPosX + 1), newPosY )
+                    // console.log("fuuuuuuusion")
+                    this.setFusionnable((newPosX + 1), newPosY, false)
+
+                }
+
+            });
+
+            if (deplacement == true) {
+                this.ajoutPion()
+
+            }
+        }
+
+        if (sens == 'g')
+        {
+
+            let liste = this.getPlacesPrises().sort(function(a,b){
+                if (a.x < b.x) {
+                    return 1
+                } else if (b.x < a.x) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            let deplacement = false
+            // console.log(liste[0].nb);
+
+            liste.forEach(element => {
+                let libre = true
+                let oldPosX = element.x
+                let oldPosY = element.y
+                let currentPosX = element.x
+                let currentPosY = element.y
+                let newPosX = element.x
+                let newPosY = element.y
+                let currentNb = this.getPlaceNb(element.x, element.y)
+
+                this.setFusionnable(element.x, element.y, true)
+                // Bon alors là ce qu'on va faire c'est qu'on va placer le "pion" le plus à droite possible
+
+                while (libre == true) {
+                    libre = this.getPlaceStatut(currentPosX, (currentPosY - 1))
+                    // console.log(currentPosX + ":" + (currentPosY - 1) + " libre ? " + libre)
+                    if (libre == true) {
+                        // console.log('oui')
+
+                        newPosY = newPosY - 1
+                        currentPosY = currentPosY - 1
+
+                    } // console.log(libre)
+
+                }
+                // console.log("sortie du while. position retenue " + newPosX + ":" + newPosY)
+                // et la on change la position
+                this.changerPosition(oldPosX, oldPosY, newPosX, newPosY)
+                // console.log(oldPosX + ":" + oldPosY + " > " + newPosX + ":" + newPosY + " " + this.getPlaceStatut(1, 1) + " " + this.pions[0].nb)
+
+                if (oldPosX != newPosX || oldPosY != newPosY) {
+                    deplacement = true
+                }
+
+                // on vérifie si la case d'après n'aurait pas le même chiffre
+                // console.log(newPosX + ":" + (newPosY + 1) + " est-il fusionnable ?")
+                // console.log(this.getFusionnable(newPosX, (newPosY + 1)))
+                if (
+                    this.getPlaceNb(newPosX, (newPosY - 1)) == currentNb && this.getFusionnable(newPosX, (newPosY - 1)) == true
+                )
+                {
+                    this.fusionnerPlaces(newPosX, newPosY, newPosX, (newPosY - 1))
+                    // console.log("fuuuuuuusion")
+                    this.setFusionnable(newPosX, (newPosY - 1), false)
+
+                }
+
+            });
+
+            if (deplacement == true) {
+                this.ajoutPion()
+
+            }
+        }
+
+        if (sens == 'd')
+        {
+
+            let liste = this.getPlacesPrises().sort(function(a,b){
+                if (a.y < b.y) {
+                    return 1
+                } else if (a.y > b.y) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            // console.log('LISTE')
+            liste.forEach(element => {
+                // console.log(element.x + ":" + element.y)
+            });
+
+            let deplacement = false
+            // console.log(liste[0].nb);
+
+            liste.forEach(element => {
+                let libre = true
+                let oldPosX = element.x
+                let oldPosY = element.y
+                let currentPosX = element.x
+                let currentPosY = element.y
+                let newPosX = element.x
+                let newPosY = element.y
+                let currentNb = this.getPlaceNb(element.x, element.y)
+
+                this.setFusionnable(element.x, element.y, true)
+                // Bon alors là ce qu'on va faire c'est qu'on va placer le "pion" le plus à droite possible
+
+                while (libre == true) {
+                    libre = this.getPlaceStatut(currentPosX, (currentPosY + 1))
+                    // console.log("libre ?")
+                    if (libre == true) {
+                        // console.log('oui')
+
+                        newPosY++
+                        currentPosY++
+
+                    } // console.log(libre)
+
+                }
+
+                // et la on change la position
+                this.changerPosition(oldPosX, oldPosY, newPosX, newPosY)
+                // console.log("position changée.")
+
+                if (oldPosX != newPosX || oldPosY != newPosY) {
+                    deplacement = true
+                }
+
+                // on vérifie si la case d'après n'aurait pas le même chiffre
+                // console.log(newPosX + ":" + (newPosY + 1) + " est-il fusionnable ?")
+                // console.log(this.getFusionnable(newPosX, (newPosY + 1)))
+                if (
+                    this.getPlaceNb(newPosX, (newPosY + 1)) == currentNb && sens == 'd' && this.getFusionnable(newPosX, (newPosY + 1)) == true
+                )
+                {
+                    this.fusionnerPlaces(newPosX, newPosY, newPosX, (newPosY + 1))
+                    // console.log("fuuuuuuusion")
+                    this.setFusionnable(newPosX, (newPosY + 1), false)
+
+                }
+
+            });
+
+            if (deplacement == true) {
+                this.ajoutPion()
+
+            }
+        }
+        
+    }
+
+    haut() {
+        this.deplacer('h')
+    }
+
+    bas() {
+        this.deplacer('b')
+    }
+
+    gauche() {
+        this.deplacer('g')
+    }
+
+    droite() {
+        this.deplacer('d')
     }
 
     /**
@@ -73,19 +506,21 @@ class Board
      * 
      * Retourne un chiffre entre 1 et la taille du plateau
      */
-    getChiffreRandom() {
-        let resultat = Math.random() * (this.size - 1) + 1;
+    getChiffreRandom(min = 1, max = this.size) {
+
+        let resultat = Math.random() * (max - min) + min;
         resultat = Math.ceil(resultat)
         return resultat
     }
 
     constructor () {
-        this.init(5)
+        this.init(4)
     }
 
     helloworld() {
-        console.log('ok ça marche !')
+        // console.log('ok ça marche !')
     }
+    
 }
 
 export default new Board();
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,8 +2,8 @@
     <div>
         <div class="plateau">
             <div v-for="(place, index) in places"
-        :key="index" :nb="nb" :index="index" class="place">
-                <p>{{place.nb}}</p>
+        :key="index" :nb="nb" :index="index" class="place" v-bind:style="{'width': dimension + 'px', 'height': dimension + 'px'}">
+                <p class="number" v-bind:nb="place.nb">{{place.nb}}</p>
             </div>
         </div>
     </div>
@@ -17,17 +17,42 @@ export default {
     methods: {
         toast () {
             // BoardJS.helloworld()
-        },
+        }
         
     },
     data () {
         return {
+            "dimension" : 500 / BoardJS.size,
             "places" : BoardJS.pions
         }
     },
     created () {
         console.log('TOAST')
         console.log(BoardJS.pions)
+
+        window.addEventListener("keypress", e => {
+            console.log('///////' + e.keyCode)
+
+            if (e.keyCode == 39) {
+                BoardJS.droite()
+                
+            }
+
+            if (e.keyCode == 37) {
+                BoardJS.gauche()
+
+            }
+
+            if (e.keyCode == 40) {
+                BoardJS.bas()
+
+            }
+
+            if (e.keyCode == 38) {
+                BoardJS.haut()
+
+            }
+        })
     }
 }
 </script>
@@ -35,15 +60,29 @@ export default {
 <style>
     .plateau
     {
+        border: solid red;
         width: 500px;
         height: 500px;
     }
 
     .place
     {
-        border: solid red;
-        width: 18%;
-        height: 18%;
+        background-color: goldenrod;
         display: inline-block;
+    }
+
+    .place p.number {
+        font-size: 2em;
+        background-color: yellow;
+        margin: 5px;
+        height: 85%;
+        border-radius: 15px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .place p.number[nb="0"] {
+        color: transparent;
     }
 </style>
